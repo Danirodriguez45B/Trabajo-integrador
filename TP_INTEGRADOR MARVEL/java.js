5c1fe2510c771b04b86872a3257ef60d3894c8ab
Danirodriguez45B <danielrodriguez45b@gmail.com>
2025-11-05 19:26:42 -0300

    Principio de Carrousel y Formulario.

--- TP_INTEGRADOR MARVEL/java.js
+++ TP_INTEGRADOR MARVEL/java.js
@@ -32,3 +32,50 @@ document.addEventListener('DOMContentLoaded', () => {
     if (principalPage) principalPage.classList.add('active');
     if (principalLink) principalLink.classList.add('active');
 });
+const marvelImages = [
+    'https://www.showmetech.com.br/wp-content/uploads//2025/03/avengers-doomsday.png', 
+    'https://sm.ign.com/t/ign_es/video/s/spider-man/spider-man-brand-new-day-official-day-one-on-set-featurette_721q.1200.jpg', 
+    'https://i0.wp.com/punchdrunkcritics.com/wp-content/uploads/2024/07/avengers-doomsday-avengers-secret-wars-scaled.jpg?fit=2560%2C1440&ssl=1', 
+    'https://media.vandal.net/m/11-2021/2021112511502752_1.jpg' 
+];
+
+let currentIndex = 0;
+const carouselImage = document.getElementById('carousel-image');
+const prevBtn = document.getElementById('prev-btn');
+const nextBtn = document.getElementById('next-btn');
+
+function updateCarousel() {
+    if (carouselImage) {
+        carouselImage.src = marvelImages[currentIndex];
+    }
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex === marvelImages.length - 1) ? 0 : currentIndex + 1;
+    updateCarousel();
+}
+
+function prevSlide() {
+    currentIndex = (currentIndex === 0) ? marvelImages.length - 1 : currentIndex - 1;
+    updateCarousel();
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    if (carouselImage && marvelImages.length > 0) {
+        updateCarousel();
+    }
+
+    if (nextBtn) {
+        nextBtn.addEventListener('click', nextSlide);
+    }
+    
+    if (prevBtn) {
+        prevBtn.addEventListener('click', prevSlide);
+    }
+
+    if (marvelImages.length > 1) {
+        setInterval(nextSlide, 5000); 
+    }
+});
